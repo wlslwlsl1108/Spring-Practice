a4fa7605f3982c8bc4f9fcca1a9f2c7ede1d35bf
jinhee kim <lsa68803@naver.com>
2025-08-29 01:20:48 +0900

    feat: [dto] 일정 요청(Request)/응답(Response) DTO 생성

--- src/main/java/com/springPractice/schedules/dto/ScheduleRequest.java
+++ src/main/java/com/springPractice/schedules/dto/ScheduleRequest.java
@@ -1,4 +1,15 @@
 package com.springPractice.schedules.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
 public class ScheduleRequest {
+
+    @NotNull
+    @Size(max = 100)
+    private String title;
+
+    private String content;
 }
--- src/main/java/com/springPractice/schedules/dto/ScheduleResponse.java
+++ src/main/java/com/springPractice/schedules/dto/ScheduleResponse.java
@@ -1,4 +1,20 @@
 package com.springPractice.schedules.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor                           // 아래 모든 필드를 매개변수로 갖는 생성자 자동 생성 -> 직접 생성할 경우 오류발생
+@JsonInclude(JsonInclude.Include.NON_NULL)    // NULL인 필드는 응답에서 제외(생략)
 public class ScheduleResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+
 }
