f8479232482888eb6043f291f9c66e5ae0b7e745
jinhee kim <lsa68803@naver.com>
2025-09-02 23:38:42 +0900

    feat: [dto] 유저 요청(Request)/응답(Response) DTO 생성

--- src/main/java/com/springPractice/users/dto/UserRequest.java
+++ src/main/java/com/springPractice/users/dto/UserRequest.java
@@ -1,4 +1,46 @@
 package com.springPractice.users.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class UserRequest {
+
+    @NotBlank(message = "유저이름은 필수 입력 값입니다.")
+    @Size(max=50, message = "50자까지 입력 가능합니다.")
+    private final String username;
+
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Size(max=100, message = "100자까지 입력 가능합니다.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private final String email;
+
 }
+/*
+     [ @NotNull ]
+        - null 값만 막음 -> null 이면 에러 발생
+        - 하지만, 빈 문자열("")이나 공백(" ") 값은 통과
+
+     [ @NotBlank ]
+        - '문자열 전용' 검증 어노테이션
+        - null,  빈 문자열(""), 공백(" ") 전부 막음
+
+     [ @NotEmpty ]
+        - null,  빈 문자열("") 막음
+        - 공백(" ")은 허용
+
+     => ERD : NOT NULL  => DB 레벨 제약 조건
+
+        즉, ERD 에서 NOT NULL 조건을 걸었다고해서
+            코드에서도 @NotNull 만 쓸 수 있는게 아님
+
+        위 어노테이션은 " 애플리케이션 레벨 검증 "
+
+        그러므로 ERD 에서 "NOT NULL" 제약조건 사용하고,
+        DTO 에서 @NotBlank 나 @NorEmpty 사용해도 문제 없다.
+ */
+
--- src/main/java/com/springPractice/users/dto/UserResponse.java
+++ src/main/java/com/springPractice/users/dto/UserResponse.java
@@ -1,4 +1,18 @@
 package com.springPractice.users.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserResponse {
+
+    private final Long userId;
+    private final String username;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+
 }
